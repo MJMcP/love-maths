8e92f71638c3ef5618a264dc152dc7d80912dac1
MJMcP <maggiejury@gmail.com>
2022-08-01 02:14:39 +0000

    Adds solution from division challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    //adds listener to allow for commiting of answer by enter button
     document.getElementById("answer-box").addEventListener("keydown", function(event){
         if (event.key === "Enter") {
             checkAnswer();
@@ -44,7 +45,9 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") { 
-        displaySubtractQuestion(num1, num2);       
+        displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);       
     } else {     
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [parseInt(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -136,5 +141,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1= operand1*operand2;
+
+	document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+	document.getElementById("operator").textContent = "/";
 
 }
